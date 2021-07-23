39b83eef9dc8915bb39e67d8cfd5b9c2ddb992ab
Puh00 <alex.phu@live.com>
2021-07-23 16:06:50 +0200

    :children_crossing: Add dropdown for choosing which algorithm to use for sorting

--- src/components/SortingVisualizer.tsx
+++ src/components/SortingVisualizer.tsx
@@ -3,18 +3,26 @@ import "../App.css";
 import { BLUE } from "../core/model/Color";
 import { Bar } from "../core/model/Bar";
 import { SelectionSort } from "../core/sorting/SelectionSort";
+import { InsertionSort } from "../core/sorting/InsertionSort";
 import { Sorter } from "../core/sorting/Sorter";
 import { finish } from "../core/sorting/Animation";
 
 function App() {
   const [bars, setBars] = useState<Bar[]>([]);
+  const [algorithm, setAlgorithm] = useState<string>("selection");
 
   useEffect(() => {
     reset();
   }, []);
 
   const sort = async (): Promise<void> => {
-    const sorter: Sorter = SelectionSort.getInstance();
+    // TODO: Fix this smelly code
+    var sorter: Sorter;
+    if (algorithm === "selection") {
+      sorter = SelectionSort.getInstance();
+    } else if (algorithm === "insertion") {
+      sorter = InsertionSort.getInstance();
+    } else return;
     await sorter.sort(bars, setBars);
     await finish(bars, setBars);
   };
@@ -30,6 +38,12 @@ function App() {
   return (
     <div>
       <div className="center">
+        <select onChange={(e) => setAlgorithm(e.target.value)}>
+          <option selected value="selection">
+            Selection sort
+          </option>
+          <option value="insertion">Insertion sort</option>
+        </select>
         <button onClick={sort}> sort! </button>
         <button onClick={reset}> reset! </button>
       </div>
