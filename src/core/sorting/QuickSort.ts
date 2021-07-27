64cb63e1d1794ca627f7ca0d176b1cea7b473ea2
Puh00 <alex.phu@live.com>
2021-07-27 22:20:35 +0200

    :dizzy: Add animation

--- src/core/sorting/QuickSort.ts
+++ src/core/sorting/QuickSort.ts
@@ -10,37 +10,55 @@ export class QuickSort extends Singleton<QuickSort>() implements Sorter {
     bars: Bar[],
     setState: React.Dispatch<React.SetStateAction<Bar[]>>
   ): Promise<void> => {
-    this._sort(bars, 0, bars.length - 1, setState);
+    await this._sort(bars, 0, bars.length - 1, setState);
   };
-  private _sort = (
+
+  private _sort = async (
     bars: Bar[],
     start: number,
     end: number,
     setState: React.Dispatch<React.SetStateAction<Bar[]>>
   ) => {
     if (start < end) {
-      let partitionIndex: number = this.partition(bars, start, end, setState);
-      this._sort(bars, start, partitionIndex - 1, setState);
-      this._sort(bars, partitionIndex + 1, end, setState);
+      let partitionIndex: number = await this.partition(
+        bars,
+        start,
+        end,
+        setState
+      );
+      await this._sort(bars, start, partitionIndex - 1, setState);
+      await this._sort(bars, partitionIndex + 1, end, setState);
     }
   };
 
-  private partition = (
+  private partition = async (
     bars: Bar[],
     start: number,
     end: number,
     setState: React.Dispatch<React.SetStateAction<Bar[]>>
-  ): number => {
+  ): Promise<number> => {
     let pivotIndex: number = end;
     let pivot = this.getMedian(bars, start, end, setState);
     let partitionIndex = start;
 
+    // Mark pivot
+    bars[pivotIndex].color = RED;
+    setState([...bars]);
+
     for (let i = start; i < end; i++) {
       if (bars[i].num <= pivot) {
         this.swap(bars, i, partitionIndex, setState);
+
+        await this.animation(bars, i, partitionIndex, start, setState);
+
         partitionIndex++;
       }
     }
+
+    // Unmark pivot and starting pointer
+    bars[start].color = BLUE;
+    bars[pivotIndex].color = BLUE;
+    setState([...bars]);
 
     this.swap(bars, partitionIndex, pivotIndex, setState);
     return partitionIndex;
@@ -82,5 +100,23 @@ export class QuickSort extends Singleton<QuickSort>() implements Sorter {
     arr[x] = arr[y];
     arr[y] = temp;
     setState([...arr]);
+  };
+
+  private animation = async (
+    bars: Bar[],
+    i: number,
+    partitionIndex: number,
+    start: number,
+    setState: React.Dispatch<React.SetStateAction<Bar[]>>
+  ): Promise<void> => {
+    bars[i].color = GREEN;
+    bars[partitionIndex].color = RED;
+    setState([...bars]);
+    await sleep(50);
+    if (i !== start) {
+      // only reset indices after start
+      bars[i].color = BLUE;
+      bars[partitionIndex].color = BLUE;
+    }
   };
 }
