954ec88fb42909e38183d4168376e92c5efaed06
Puh00 <alex.phu@live.com>
2021-07-28 16:22:52 +0200

    :dizzy: Animation for Merge Sort

--- src/core/sorting/MergeSort.ts
+++ src/core/sorting/MergeSort.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { Sorter } from "./Sorter";
 import { Bar } from "../model/Bar";
 import { sleep } from "../../utils/Sleep";
-import { RED, GREEN, BLUE } from "../model/Color";
+import { RED, BLUE } from "../model/Color";
 import { Singleton } from "../../utils/Singleton";
 
 export class MergeSort extends Singleton<MergeSort>() implements Sorter {
@@ -28,6 +28,16 @@ export class MergeSort extends Singleton<MergeSort>() implements Sorter {
     await this.inPlaceMerge(bars, start, end, setState);
   };
 
+  /**
+   * In-place Merge Sort
+   *
+   * Approach 2 from {@link https://www.geeksforgeeks.org/in-place-merge-sort/}.
+   *
+   * @param bars The array to be sorted.
+   * @param start The left index of the array.
+   * @param end The right index of the array.
+   * @param setState Hook for changing the state.
+   */
   private inPlaceMerge = async (
     bars: Bar[],
     start: number,
@@ -39,7 +49,10 @@ export class MergeSort extends Singleton<MergeSort>() implements Sorter {
     for (gap = this.nextGap(gap); gap > 0; gap = this.nextGap(gap)) {
       for (let i = start; i + gap <= end; i++) {
         let j = i + gap;
-        if (bars[i].num > bars[j].num) await this.swap(bars, i, j, setState);
+        if (bars[i].num > bars[j].num) {
+          await this.swap(bars, i, j, setState);
+          await this.animation(bars, i, j, setState);
+        }
       }
     }
   };
@@ -57,6 +70,21 @@ export class MergeSort extends Singleton<MergeSort>() implements Sorter {
     arr[x] = arr[y];
     arr[y] = temp;
     setState([...arr]);
+  };
+
+  private animation = async (
+    bars: Bar[],
+    x: number,
+    y: number,
+    setState: React.Dispatch<React.SetStateAction<Bar[]>>
+  ): Promise<void> => {
+    bars[x].color = RED;
+    bars[y].color = RED;
+
+    setState([...bars]);
     await sleep(100);
+
+    bars[x].color = BLUE;
+    bars[y].color = BLUE;
   };
 }
