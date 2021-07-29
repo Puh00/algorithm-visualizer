feec254a951700263518d744eae66947ec04b723
Puh00 <alex.phu@live.com>
2021-07-30 00:32:25 +0200

    :rotating_light: Fix no-use-before-define warning

--- src/core/sorting/QuickSort.ts
+++ src/core/sorting/QuickSort.ts
@@ -1,34 +1,64 @@
-import React from "react";
-import { Bar } from "../model/Bar";
-import { sleep } from "../../utils/Sleep";
-import { RED, GREEN, BLUE } from "../model/Color";
+import React from 'react';
 
-export const quicksort = async (
+import { sleep } from '../../utils/Sleep';
+import { Bar } from '../model/Bar';
+import { RED, GREEN, BLUE } from '../model/Color';
+
+const swap = (
+  arr: Bar[],
+  x: number,
+  y: number,
+  setState: React.Dispatch<React.SetStateAction<Bar[]>>
+): void => {
+  const temp: Bar = arr[x];
+  arr[x] = arr[y];
+  arr[y] = temp;
+  setState([...arr]);
+};
+
+const animation = async (
   bars: Bar[],
+  i: number,
+  partitionIndex: number,
+  start: number,
   setState: React.Dispatch<React.SetStateAction<Bar[]>>,
   delay: number
 ): Promise<void> => {
-  await _sort(bars, 0, bars.length - 1, setState, delay);
+  bars[i].color = GREEN;
+  bars[partitionIndex].color = RED;
+  setState([...bars]);
+  await sleep(delay);
+  if (i !== start) {
+    // only reset indices after start
+    bars[i].color = BLUE;
+    bars[partitionIndex].color = BLUE;
+  }
 };
 
-const _sort = async (
+/**
+ * Median of three
+ *
+ * Puts the median at the last index of the array and returns the pivot value
+ *
+ * @param bars The array to be sorted.
+ * @param start The left index of the array.
+ * @param end The right index of the array.
+ * @returns The pivot value.
+ */
+const getMedian = (
   bars: Bar[],
   start: number,
   end: number,
-  setState: React.Dispatch<React.SetStateAction<Bar[]>>,
-  delay: number
-) => {
-  if (start < end) {
-    let partitionIndex: number = await partition(
-      bars,
-      start,
-      end,
-      setState,
-      delay
-    );
-    await _sort(bars, start, partitionIndex - 1, setState, delay);
-    await _sort(bars, partitionIndex + 1, end, setState, delay);
-  }
+  setState: React.Dispatch<React.SetStateAction<Bar[]>>
+): number => {
+  const midpoint: number = Math.floor((start + end) / 2);
+  if (bars[start].num > bars[midpoint].num)
+    swap(bars, start, midpoint, setState);
+  if (bars[start] > bars[end]) swap(bars, start, end, setState);
+  if (bars[midpoint] > bars[end]) swap(bars, midpoint, end, setState);
+
+  swap(bars, midpoint, end, setState);
+  return bars[end].num;
 };
 
 const partition = async (
@@ -38,8 +68,8 @@ const partition = async (
   setState: React.Dispatch<React.SetStateAction<Bar[]>>,
   delay: number
 ): Promise<number> => {
-  let pivotIndex: number = end;
-  let pivot = getMedian(bars, start, end, setState);
+  const pivotIndex: number = end;
+  const pivot = getMedian(bars, start, end, setState);
   let partitionIndex = start;
 
   // Mark pivot
@@ -65,59 +95,30 @@ const partition = async (
   return partitionIndex;
 };
 
-/**
- * Median of three
- *
- * Puts the median at the last index of the array and returns the pivot value
- *
- * @param bars The array to be sorted.
- * @param start The left index of the array.
- * @param end The right index of the array.
- * @returns The pivot value.
- */
-const getMedian = (
+const _sort = async (
   bars: Bar[],
   start: number,
   end: number,
-  setState: React.Dispatch<React.SetStateAction<Bar[]>>
-): number => {
-  let midpoint: number = Math.floor((start + end) / 2);
-  if (bars[start].num > bars[midpoint].num)
-    swap(bars, start, midpoint, setState);
-  if (bars[start] > bars[end]) swap(bars, start, end, setState);
-  if (bars[midpoint] > bars[end]) swap(bars, midpoint, end, setState);
-
-  swap(bars, midpoint, end, setState);
-  return bars[end].num;
-};
-
-const swap = (
-  arr: Bar[],
-  x: number,
-  y: number,
-  setState: React.Dispatch<React.SetStateAction<Bar[]>>
-): void => {
-  let temp: Bar = arr[x];
-  arr[x] = arr[y];
-  arr[y] = temp;
-  setState([...arr]);
-};
-
-const animation = async (
-  bars: Bar[],
-  i: number,
-  partitionIndex: number,
-  start: number,
   setState: React.Dispatch<React.SetStateAction<Bar[]>>,
   delay: number
 ): Promise<void> => {
-  bars[i].color = GREEN;
-  bars[partitionIndex].color = RED;
-  setState([...bars]);
-  await sleep(delay);
-  if (i !== start) {
-    // only reset indices after start
-    bars[i].color = BLUE;
-    bars[partitionIndex].color = BLUE;
+  if (start < end) {
+    const partitionIndex: number = await partition(
+      bars,
+      start,
+      end,
+      setState,
+      delay
+    );
+    await _sort(bars, start, partitionIndex - 1, setState, delay);
+    await _sort(bars, partitionIndex + 1, end, setState, delay);
   }
+};
+
+export const quicksort = async (
+  bars: Bar[],
+  setState: React.Dispatch<React.SetStateAction<Bar[]>>,
+  delay: number
+): Promise<void> => {
+  await _sort(bars, 0, bars.length - 1, setState, delay);
 };
