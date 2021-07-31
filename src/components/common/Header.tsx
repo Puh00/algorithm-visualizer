224869c75278e9e8f5dd2fc1f85061c57cfda369
Puh00 <alex.phu@live.com>
2021-07-31 22:50:24 +0200

    Add Github logo

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import {
+  Container,
+  Nav,
+  Navbar,
+  OverlayTrigger,
+  Tooltip,
+} from 'react-bootstrap';
+
+import logo from '../../assets/github.png';
 
 export const Header = (): JSX.Element => {
   return (
     <Navbar expand="md" bg="dark" variant="dark" className="app-vanish">
       <Container>
-        <Navbar.Brand href="/">〽️</Navbar.Brand>
+        <Navbar.Brand href="https://github.com/Puh00/algorithm-visualizer">
+          <OverlayTrigger
+            placement="bottom"
+            overlay={<Tooltip id="tip">View Project</Tooltip>}
+          >
+            <img src={logo} alt="github logo" />
+          </OverlayTrigger>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
