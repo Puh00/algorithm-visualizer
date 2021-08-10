6c15b23f0b8290fb5945f95c25ab8e221093768c
Puh00 <alex.phu@live.com>
2021-08-10 10:59:48 +0200

    :memo: Add documentation to utils

--- src/utils/Coordinates.ts
+++ src/utils/Coordinates.ts
@@ -4,7 +4,14 @@ import { Coord } from '../core/model/Cell';
 const yDir = [-1, 0, 1, 0];
 const xDir = [0, 1, 0, -1];
 
-// Returns adjacent coordinates that are within bounds
+/**
+ * Retrieves adjacent coordinates that shares a border with the given coordinate.
+ * @param c the coordinate to evaluate.
+ * @param n the height of the grid.
+ * @param m the width of the grid.
+ * @param offset by default, the function will only give neighbours that are 1 unit away.
+ * @returns adjacent coordinates that are within bounds.
+ */
 export const adjacentCoords = (
   c: Coord,
   n: number,
@@ -28,6 +35,14 @@ export const isSameCoord = (c1: Coord, c2: Coord): boolean =>
 
 type dir = 'HORIZONTAL' | 'VERTICAL';
 
+/**
+ * Calculates the alignement between two coordinates.
+ * @param p the first coordinate.
+ * @param q the second coordinate.
+ * @throws will throw an error if the coordinates are neither vertically or
+ * horizontally aligned
+ * @returns the alignment between the coordinates.
+ */
 export const alignmentBetweenCoordinates = (p: Coord, q: Coord): dir => {
   if (Math.max(p.y, q.y) - Math.min(p.y, q.y) === 0) return 'HORIZONTAL';
   else if (Math.max(p.x, q.x) - Math.min(p.x, q.x) === 0) return 'VERTICAL';
--- src/utils/Maze.ts
+++ src/utils/Maze.ts
@@ -1,6 +1,11 @@
 import { alignmentBetweenCoordinates, sleep } from '.';
 import { Cell, Coord } from '../core/model/Cell';
 
+/**
+ * Convert each cell in the grid to a wall.
+ * @param grid the grid that has the state of every cell.
+ * @param setGrid react hook to update the state of the grid.
+ */
 export const markAllCellsAsWalls = (
   grid: Cell[][],
   setGrid: React.Dispatch<React.SetStateAction<Cell[][]>>
@@ -13,28 +18,38 @@ export const markAllCellsAsWalls = (
   setGrid([...grid]);
 };
 
+/**
+ * Carves to the right from the given coordinate.
+ * @param grid the grid to carve upon.
+ * @param x x coordinate.
+ * @param y y coordinate.
+ * @param setGrid react hook to update the state of the grid.
+ */
 export const carveHorizontaly = async (
   grid: Cell[][],
   x: number,
   y: number,
   setGrid: React.Dispatch<React.SetStateAction<Cell[][]>>
 ): Promise<void> => {
-  for (let i = 0; i < 3; i++) {
-    grid[y][x + i].isWall = false;
-  }
+  for (let i = 0; i < 3; i++) grid[y][x + i].isWall = false;
   setGrid([...grid]);
   await sleep(1);
 };
 
+/**
+ * Carves downwards from the given coordinate.
+ * @param grid the grid to carve upon.
+ * @param x x coordinate.
+ * @param y y coordinate.
+ * @param setGrid react hook to update the state of the grid.
+ */
 export const carveVertically = async (
   grid: Cell[][],
   x: number,
   y: number,
   setGrid: React.Dispatch<React.SetStateAction<Cell[][]>>
 ): Promise<void> => {
-  for (let i = 0; i < 3; i++) {
-    grid[y + i][x].isWall = false;
-  }
+  for (let i = 0; i < 3; i++) grid[y + i][x].isWall = false;
   setGrid([...grid]);
   await sleep(1);
 };
@@ -42,7 +57,6 @@ export const carveVertically = async (
 /**
  * Carve a passage between two coordinates if and only if they are adjacent
  * and vertically or horizontally aligned.
- *
  * @param p starting coordinate.
  * @param q target coordinate.
  * @param grid the grid to carve upon.
@@ -55,11 +69,10 @@ export const carvePassageBetweenAdjacentCoordinates = async (
   setGrid: React.Dispatch<React.SetStateAction<Cell[][]>>
 ): Promise<void> => {
   const alignment = alignmentBetweenCoordinates(p, q);
-  if (alignment === 'HORIZONTAL') {
+  if (alignment === 'HORIZONTAL')
     // current cell and neighbour are horizontally aligned
     await carveHorizontaly(grid, Math.min(p.x, q.x), p.y, setGrid);
-  } else if (alignment === 'VERTICAL') {
+  else if (alignment === 'VERTICAL')
     // ...vertically aligned
     await carveVertically(grid, p.x, Math.min(p.y, q.y), setGrid);
-  }
 };
